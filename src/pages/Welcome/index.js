f34608615bea637834ec367fe6bb16afad4ae2cb
Victor-Sarti <sartivictor24@gmail.com>
2024-09-15 19:44:26 -0300

    feat: install and import biblioteca native-animatable

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
-
+import * as Animatable from 'react-native-animatable'
 const Welcome = () => {
     return (
         <View style={styles.container}>

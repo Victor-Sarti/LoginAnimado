951a1b1acb109125908719365d3eecda1d84ccd8
Victor-Sarti <sartivictor24@gmail.com>
2024-09-15 18:56:41 -0300

    install react navigation config routes

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Welcome = () => {
+    return (
+        <View>
+            <Text>Tela Bem vindo</Text>
+        </View>
+      );
+}
+ 
+export default Welcome;

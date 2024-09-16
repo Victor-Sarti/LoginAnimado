ed8a76fc883b2360c99536eaeded5e17d2797330
Victor-Sarti <sartivictor24@gmail.com>
2024-09-15 22:13:12 -0300

    feat:add bem vindo in SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
-
-const SignIn = () => {
+import { View, Text, StyleSheet } from "react-native";
+import * as Animatable from 'react-native-animatable'
+export default function SignIn(){
     return (
-        <View>
-            <Text>Tela Login</Text>
+        <View styles={styles.container}>
+          <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+            <Text style={styles.message}> Bem-Vindo(a)</Text>
+          </Animatable.View>
+
         </View>
       );
 }
  
-export default SignIn;
+const styles = StyleSheet.create({
+    container:{
+
+    }
+}
+    
+)
